e6345817d0fa6c42201c15bd97acfa4d49127e07
Mithgol the Webmaster <getgit@mithgol.ru>
2015-08-06 23:45:46 +0300

    +2 optional parameters: input and output encodings

--- api.js
+++ api.js
@@ -3,10 +3,11 @@ var path = require('path');
 
 require('iconv-lite').extendNodeEncodings();
 
-var readFileOrDie = function(filename, rusMode){
+var readFileOrDie = function(filename, encoding, rusMode){
    try {
-      return fs.readFileSync(filename, {encoding: 'cp866'});
+      return fs.readFileSync(filename, {encoding: encoding});
    } catch(err) {
+      console.log('');
       if( rusMode ){
          console.log([
             'Файл ',
@@ -20,14 +21,19 @@ var readFileOrDie = function(filename, rusMode){
             ' cannot be read.'
          ].join(''));
       }
+      console.log('');
       throw err;
    }
 };
 
-module.exports = function(filenameCSV, filenameHPT, rusMode){
+module.exports = function(filenameCSV, filenameHPT, options){
    filenameCSV = path.resolve(__dirname, filenameCSV);
    filenameHPT = path.resolve(__dirname, filenameHPT);
 
-   var contentCSV = readFileOrDie(filenameCSV);
-   var contentHPT = readFileOrDie(filenameHPT);
+   var contentCSV = readFileOrDie(
+      filenameCSV, options.inputEncoding, options.rusMode
+   );
+   var contentHPT = readFileOrDie(
+      filenameHPT, options.outputEncoding, options.rusMode
+   );
 };
--- echolist-csv2hpt.js
+++ echolist-csv2hpt.js
@@ -7,9 +7,17 @@ var params = [].concat(process.argv);
 params.shift(); // 'node'
 params.shift(); // 'echolist-csv2hpt'
 
+var inputEncoding = 'cp866';
+var outputEncoding = 'cp866';
 var rusMode = false;
 params = params.filter(function(nextParam){
-   if( nextParam.toLowerCase() === '--rus' ){
+   if( nextParam.indexOf('--input=') === 0 ){
+      inputEncoding = nextParam.slice('--input='.length);
+      return false;
+   } else if( nextParam.indexOf('--output=') === 0 ){
+      outputEncoding = nextParam.slice('--output='.length);
+      return false;
+   } else if( nextParam.toLowerCase() === '--rus' ){
       rusMode = true;
       return false;
    }
@@ -33,6 +41,15 @@ if( params.length < 2 ){
       clog('употребление русских сообщений вместо английских.');
       clog('');
       clog('(Воздействует только на вывод в консоли, не на выходной файл.)');
+      clog('');
+      clog('Необязательные параметры "--input=CP866" и "--output=CP866"');
+      clog('задают кодировки входного (CSV) и выходного (HPT) файла.');
+      clog('Вместо CP866 в этих параметрах можно употреблять кодировки,');
+      clog('https://github.com/ashtuchkin/iconv-lite поддерживаемые.');
+      clog('');
+      clog('По умолчанию используется кодировка CP866 (обычная на системе');
+      clog('Windows в российском Фидонете, так что она употребляется');
+      clog('наиболее часто).');
    } else {
       clog('Usage:');
       clog('   echolist-csv2hpt inputCSV configHPT');
@@ -43,10 +60,18 @@ if( params.length < 2 ){
       clog('');
       clog('configHPT -- path to the HPT echomail area configuration file.');
       clog('');
-      clog('An optional "--rus" parameter tells the application to use');
+      clog('An optional "--rus" parameter dictates the application to use');
       clog('Russian messages instead of English.');
       clog('');
       clog('(It only affects console output, not the output file.)');
+      clog('');
+      clog('Optional parameters "--input=CP866" and "--output=CP866" define');
+      clog('the encodings of input (CSV) and output (HPT) file. Encodings');
+      clog('supported by https://github.com/ashtuchkin/iconv-lite can be');
+      clog('used in these parameters instead of CP866.');
+      clog('');
+      clog('CP866 is the default encoding (it is used by Windows users');
+      clog('in Russian Fidonet, which is the largest use case).');
    }
    process.exit(1);
 }
@@ -54,4 +79,12 @@ if( params.length < 2 ){
 var filenameCSV = params[0];
 var filenameHPT = params[1];
 
-thisAPI(filenameCSV, filenameHPT, rusMode);
+thisAPI(
+   filenameCSV,
+   filenameHPT,
+   {
+      rusMode: rusMode,
+      inputEncoding: inputEncoding,
+      outputEncoding: outputEncoding
+   }
+);
